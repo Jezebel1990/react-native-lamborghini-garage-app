bc0c322fb17eb414b968707f5d919d9f984a7731
Jezebel1990 <jezebelguedes@gmail.com>
2025-09-04 01:14:29 -0300

    feat: add reder car Image

--- src/components/CardView/index.tsx
+++ src/components/CardView/index.tsx
@@ -4,6 +4,7 @@ import { View, Text, Button, Image } from "react-native";
 import Logo from "../../../assets/logo.png";
 import { styles } from './style';
 import Divider from '../Divider';
+import { CAR_ASSETS_BASE_URL } from '../../constants/car';
 
 export default function CardView() {
     const renderLogoBox = () => (
@@ -12,10 +13,28 @@ export default function CardView() {
       </View>
     );
 
+    const renderCarDetails = () => (
+    <View style={{ alignItems: "center" }}>
+      <Text style={styles.carBrand}>Lamborghini</Text>
+      <Text style={styles.carName}>Model</Text>
+    </View>
+  );
+
+   const renderCarImage = () => (
+    <Image
+      style={styles.image}
+      source={{
+        uri: `${CAR_ASSETS_BASE_URL}5.png`,
+      }}
+    />
+  );
+
     return (
         <View style={styles.imageContainer}>
           {renderLogoBox()}
           <Divider/>
+          {renderCarDetails()}
+          {renderCarImage()}
         </View>
     );
 }
