736042b81d441a6f9aa7de4732acaceb1c6c22fe
Jezebel1990 <jezebelguedes@gmail.com>
2025-09-04 00:51:42 -0300

    feat: create sub components

---
+++ src/components/CardView/index.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { View, Text, Button, Image } from "react-native";
+
+import Logo from "../../../assets/logo.png";
+import { styles } from './style';
+import Divider from '../Divider';
+
+export default function CardView() {
+    const renderLogoBox = () => (
+     <View style={styles.logoContainer}>
+      <Image style={styles.imageLogo} source={Logo} />
+      </View>
+    );
+
+    return (
+        <View style={styles.imageContainer}>
+          {renderLogoBox()}
+          <Divider/>
+        </View>
+    );
+}
